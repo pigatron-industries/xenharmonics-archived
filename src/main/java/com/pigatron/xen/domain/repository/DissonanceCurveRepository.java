585ed14985a5995456e9471d4f09db8a3ba30dd8
Robert Ellis <rob@pigatron-industries.co.uk>
2016-02-13 14:19:28 +0000

    Corrected Typo

--- src/main/java/com/pigatron/xen/domain/repository/DissonanceCurveRepository.java
+++ src/main/java/com/pigatron/xen/domain/repository/DissonanceCurveRepository.java
@@ -4,5 +4,5 @@ package com.pigatron.xen.domain.repository;
 import com.pigatron.xen.domain.entity.DissonanceCurve;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface DissonanaceCurveRepository extends MongoRepository<DissonanceCurve, String> {
+public interface DissonanceCurveRepository extends MongoRepository<DissonanceCurve, String> {
 }
